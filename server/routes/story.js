19b4a7173597ee5c8fc2bc97a26f183e3a9f940c
Harsh311003 <harshsharma311003@gmail.com>
2023-03-25 12:06:53 +0530

    Adding backend api of allstory ,mystory

---
+++ server/routes/story.js
@@ -0,0 +1,57 @@
+const express = require("express"); 
+const router = express.Router();
+const mongoose = require('mongoose');
+const requireLogin=require("../middleware/requireLogin");
+const Story=mongoose.model("Story");
+
+//Create new story 
+router.post("/createstory",requireLogin,(req,res)=>{
+    const {title,body,pic,type}=req.body;
+    if(!title || !body || !pic || !type )
+    {
+        return res.status(422).json({error :"Please add all the fields"});
+    }
+      req.user.password=undefined    // to remove the password from post
+     
+    const story=new Story({
+        title,
+        body,
+        photo:pic,
+        category:type,
+        postedBy:req.user
+    })
+
+    story.save().then(result=>{
+        res.json({story:result})
+    })
+    .catch(err=>{
+        console.log(err)
+    });
+   
+});
+
+//for all stoies
+router.get("/allstory",requireLogin,(req,res)=>{
+    Story.find()                         //finding all posts  without any condition
+    .populate("postedBy","_id name")
+    .populate("comments.postedBy","_id name")  //   showing _id and name in postedBy
+    .sort('-createdAt')
+    .then(stories=>{
+        res.json({stories:stories})   
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+//  my uploaded stories
+router.get("/mystory",requireLogin,(req,res)=>{  
+    Story.find({postedBy: req.user._id})  
+    .populate("postedBy","_id name")  //it will expand only _id and name
+    .then(mystory=>{
+        res.json({mystory})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+module.exports=router;
