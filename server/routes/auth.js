7cb1c26695807b091408e60dd1bf670a25b01612
Nupur Goyal <nupur102002@gmail.com>
2023-03-25 22:20:48 +0530

    backend for new password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -200,6 +200,29 @@ router.post('/reset-password',(req,res)=>{
 })
 
 
+//user new-password
+router.post('/new-password',(req,res)=>{
+    const newPassword = req.body.password
+    const sentToken = req.body.token
+    User.findOne({resetToken:sentToken,expireToken:{$gt:Date.now()}})
+    .then(user=>{
+        if(!user){
+            return res.status(422).json({error:"Try again session expired"})
+        }
+        bcrypt.hash(newPassword,12).then(hashedpassword=>{
+           user.password = hashedpassword
+           user.resetToken = undefined
+           user.expireToken = undefined
+           user.save().then((result)=>{
+               res.json({message:"password updated success"})
+           })
+        })
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
+
 
 
 
