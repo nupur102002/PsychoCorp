985c702cf7af026932c37dcf5f452cf6de72ac9d
Harsh311003 <harshsharma311003@gmail.com>
2023-03-25 23:01:27 +0530

    follow backend connected

--- server/routes/doctor.js
+++ server/routes/doctor.js
@@ -9,6 +9,28 @@ const Doctor = mongoose.model("Doctor");
 const User = mongoose.model("User");
 
 
+/// user follow the doctor
+router.put('/follow',requireLogin,(req,res)=>{
+    Doctor.findByIdAndUpdate(req.body.followId,{
+        $push:{followers:req.user._id}
+    },{
+        new:true
+    },(err,result1)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }
+      User.findByIdAndUpdate(req.user._id,{
+          $push:{following:req.body.followId}
+          
+      },{new:true}).select("-password").then(result2=>{
+          res.json({result1,result2})
+      }).catch(err=>{
+          return res.status(422).json({error:err})
+      })
+
+    }
+    )
+})
 /// doctor profile when user click on doctor information
 router.get('/doctorprofile/:id',requireLoginDoc,(req,res)=>{      // here we get the "id" of the user along with url , whose profile we want to see
     Doctor.findOne({_id:req.params.id})                   // query to find the user using "id" in "User" db
