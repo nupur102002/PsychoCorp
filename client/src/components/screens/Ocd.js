205e8bfd16f73edbd0340a932360d98a80f71a2b
Nupur Goyal <nupur102002@gmail.com>
2023-03-26 02:47:14 +0530

    show ocd category doctors

--- client/src/components/screens/Ocd.js
+++ client/src/components/screens/Ocd.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useContext } from "react";
 import { UserContext } from '../../App';
 import { Link } from "react-router-dom";
+import { Row, Col } from "antd"
 import M from "materialize-css"
 
 const Ocd = () => {
   const [data, setData] = useState([])
   const [commentValue, setcommentValue] = useState("")
   const { state, dispatch } = useContext(UserContext)
+  const [doc, setDoc] = useState([])
+
   useEffect(() => {
     fetch('/getocd', {
       headers: {
@@ -16,6 +19,18 @@ const Ocd = () => {
       .then(result => {
         // console.log(result)
         setData(result.stories)
+      })
+  }, [])
+
+  useEffect(() => {
+    fetch('/getocddoc', {
+      headers: {
+        "Authorization": "Bearer " + localStorage.getItem("jwt")
+      }
+    }).then(res => res.json())
+      .then(result => {
+        console.log(result)
+        setDoc(result.doctors)
       })
   }, [])
 
@@ -136,58 +151,80 @@ const Ocd = () => {
             OCD has been shown to respond well to psychotherapy as well as medication. Specifically, cognitive-behavioral therapy (CBT) and exposure therapy have the most research supporting their effectiveness. It’s normal, on occasion to double-check if the stove is off, or to be concerned about the well-being of others. But if these thoughts or behaviors become excessive and begin to interfere with your ability to lead a normal life, it may be time to get help.</p></div>
         <div><img style={{ width: "300px", height: "200px", paddingRight: "20px" }} src="https://support.therapytribe.com/wp-content/uploads/2016/06/Obsessive-Compulsive-Disorder-Full-1038x693.jpg" />
           <br />
-          <button className='btn'>Find a therapist</button></div>
+
+        </div>
       </div>
-      <hr/>
-            <h3 style={{ textAlign: "center" }}>SUCCESS STORIES THAT MAY INSPIRE YOU!!</h3>
+      <hr />
+      <div>
+        <Row justify="center mt-1" gutter={16}>
+          {doc.map((doctor) => {
+            return (
+              <Col lg={5} sm={24} xs={24}>
+                <Link to="/doctorprofile/{doctor.id}"><div className="car p-2 bs1 mt-3">
+                  <div><img style={{ borderRadius: "50%", width: "200px", paddingLeft: "50px" }} src={doctor.pic} alt="a doctor" className="carimg" /></div>
 
-            <div className="home">
-                {
-                    data.map(item => {
-                        return (
+                  <div className="car-content d-flex align-items-center justify-content-between">
+                    <div>
+                      <p className="pa" align="center">{doctor.name}</p>
+                      <p className="pa" align="center">{doctor.body}</p>
+                    </div>
+                  </div>
+                </div></Link>
+              </Col>
+            );
+          })}
+        </Row>
+      </div>
+      <hr />
+      <h3 style={{ textAlign: "center" }}>SUCCESS STORIES THAT MAY INSPIRE YOU!!</h3>
 
-                            <div className="card home-card" key={item._id}>
-                                <h5><Link to={item.postedBy._id !== state._id ? "/profile/" + item.postedBy._id : "/profile"} >{item.postedBy.name}</Link> {item.postedBy._id == state._id
-                                    && <i className="material-icons" style={{
-                                        float: "right"
-                                    }}
-                                        onClick={() => deletePost(item._id)}
-                                    >delete</i>
-                                }</h5>
-                                <div className="card-image">
-                                        <img src={item.photo} alt={item.title}/>
-                                </div>
-                                <div className="card-content">
-                                    <i className="material-icons" style={{ color: "red" }}>favorite</i>
-                                    {item.likes.includes(state._id)
-                                        ?
-                                        <i className="material-icons" onClick={() => { unlikePost(item._id) }}>thumb_down</i>
-                                        :
-                                        <i className="material-icons" onClick={() => { likePost(item._id) }}>thumb_up</i>
-                                    }
-                                    <h6>{item.likes.length} likes</h6>
-                                    <h6>{item.title}</h6>
-                                    <p>{item.body}</p>
-                                    {
-                                        item.comments.map(record => {
-                                            return (
-                                                <h6 key={record._id}><span style={{ fontWeight: "500" }}>{record.postedBy.name} </span>{record.text}</h6>
-                                            )
-                                        })
-                                    }
-                                    <form onSubmit={(e) => {
-                                        e.preventDefault()
-                                        makeComment(e.target[0].value, item._id)
-                                    }}>
-                                        <input value={commentValue} onChange={(e) => setcommentValue(e.target.value)} type="text" placeholder="add a comment" />
-                                    </form>
-                                </div>
-                            </div>
+      <div className="home">
+        {
+          data.map(item => {
+            return (
 
-                        )
+              <div className="card home-card" key={item._id}>
+                <h5><Link to={item.postedBy._id !== state._id ? "/profile/" + item.postedBy._id : "/profile"} >{item.postedBy.name}</Link> {item.postedBy._id == state._id
+                  && <i className="material-icons" style={{
+                    float: "right"
+                  }}
+                    onClick={() => deletePost(item._id)}
+                  >delete</i>
+                }</h5>
+                <div className="card-image">
+                  <img src={item.photo} alt={item.title} />
+                </div>
+                <div className="card-content">
+                  <i className="material-icons" style={{ color: "red" }}>favorite</i>
+                  {item.likes.includes(state._id)
+                    ?
+                    <i className="material-icons" onClick={() => { unlikePost(item._id) }}>thumb_down</i>
+                    :
+                    <i className="material-icons" onClick={() => { likePost(item._id) }}>thumb_up</i>
+                  }
+                  <h6>{item.likes.length} likes</h6>
+                  <h6>{item.title}</h6>
+                  <p>{item.body}</p>
+                  {
+                    item.comments.map(record => {
+                      return (
+                        <h6 key={record._id}><span style={{ fontWeight: "500" }}>{record.postedBy.name} </span>{record.text}</h6>
+                      )
                     })
-                }
-            </div>
+                  }
+                  <form onSubmit={(e) => {
+                    e.preventDefault()
+                    makeComment(e.target[0].value, item._id)
+                  }}>
+                    <input value={commentValue} onChange={(e) => setcommentValue(e.target.value)} type="text" placeholder="add a comment" />
+                  </form>
+                </div>
+              </div>
+
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
