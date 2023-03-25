d788701c892798808fb0bd9130eff379cde6eae0
Nupur Goyal <nupur102002@gmail.com>
2023-03-25 20:34:47 +0530

    fetch comment on stories

--- client/src/components/screens/Addiction.js
+++ client/src/components/screens/Addiction.js
@@ -75,6 +75,35 @@ const Addiction = () => {
                 console.log(err)
             })
     }
+
+    const makeComment = (text, postId) => {
+        fetch('/comment', {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            },
+            body: JSON.stringify({
+                postId,
+                text
+            })
+        }).then(res => res.json())
+            .then(result => {
+                console.log(result)
+                const newData = data.map(item => {
+                    if (item._id == result._id) {
+                        return result;
+                    }
+                    else {
+                        return item;
+                    }
+                })
+                setData(newData)
+                setcommentValue("")
+            }).catch(err => {
+                console.log(err)
+            })
+    }
     return (
         <div>
             <div className="add">
