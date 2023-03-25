dc72e49eb114590fd5d45c9fb5d8bfd960a7cd0c
Harsh311003 <harshsharma311003@gmail.com>
2023-03-25 15:50:47 +0530

    Adding routing and usereducer created

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,54 @@
 import './App.css';
+import React,{useEffect,createContext,useReducer,useContext} from 'react';
+import {BrowserRouter,Routes,Route,useNavigate,useLocation} from "react-router-dom"
+import {reducer,initialState} from './reducers/userReducer'
+
 import Navbar from "./components/Navbar"
-import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Home from "./components/screens/Home"
 import SignIn from "./components/screens/Login"
 import Profile from "./components/screens/Profile"
 import SignUp from "./components/screens/Signup"
 import CreatePost from "./components/screens/CreatePost"
+export const UserContext = createContext()
+
+
+const Routing = ()=>{
+  const navigate = useNavigate()
+  const location = useLocation()
+  const {state,dispatch} = useContext(UserContext)
+  useEffect(()=>{
+    const user = JSON.parse(localStorage.getItem("user"))
+    if(user){
+      console.log(state)
+      console.log(user)
+      console.log(user.usertype)
+      dispatch({type:"USER",payload:user})
+    }else{
+      if(!location.pathname.startsWith('/reset') &&!location.pathname.startsWith('/docreset' ))
+      navigate("/health")
+    }
+  },[])
+
+  return(
+    <Routes>
+    <Route exact path="/" element={<Home />} />
+    <Route path="/login" element={<SignIn />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/signup" element={<SignUp />} />
+    <Route path="/create" element={<CreatePost />} />     
+  </Routes>
+  )
+}
 function App() {
+  const [state,dispatch] = useReducer(reducer,initialState)
   return (
+    <UserContext.Provider value={{state,dispatch}}>
     <BrowserRouter>
-    <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/create" element={<CreatePost />} />     
-      </Routes>
-</BrowserRouter>
+      <Navbar />
+      <Routing />  
+     
+    </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
