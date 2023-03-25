f6cbf5806e7ce257bc70706fdecd16f311ffe003
Nupur Goyal <nupur102002@gmail.com>
2023-03-25 20:54:45 +0530

    changes

--- client/src/components/screens/Lgbtq.js
+++ client/src/components/screens/Lgbtq.js
@@ -5,6 +5,119 @@ import "react-html5video/dist/styles.css"
 import M from "materialize-css"
 
 const Lgbtq = () => {
+  const [data, setData] = useState([])
+  const [commentValue, setcommentValue] = useState("")
+  const { state, dispatch } = useContext(UserContext)
+  useEffect(() => {
+      fetch('/getlgbtq', {
+          headers: {
+              "Authorization": "Bearer " + localStorage.getItem("jwt")
+          }
+      }).then(res => res.json())
+          .then(result => {
+              // console.log(result)
+              setData(result.stories)
+          })
+  }, [])
+
+  const likePost = (id) => {
+      fetch('/like', {
+          method: "put",
+          headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + localStorage.getItem("jwt")
+          },
+          body: JSON.stringify({
+              postId: id
+          })
+      }).then(res => res.json())
+          .then(result => {
+              console.log(result)
+              const newData = data.map(item => {
+                  if (item._id == result._id) {
+                      return result;
+                  }
+                  else {
+                      return item;
+                  }
+              })
+              setData(newData)
+          }).catch(err => {
+              console.log(err)
+          })
+  }
+
+  const unlikePost = (id) => {
+      fetch('/unlike', {
+          method: "put",
+          headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + localStorage.getItem("jwt")
+          },
+          body: JSON.stringify({
+              postId: id
+          })
+      }).then(res => res.json())
+          .then(result => {
+
+              const newData = data.map(item => {
+                  if (item._id == result._id) {
+                      return result;
+                  }
+                  else {
+                      return item;
+                  }
+              })
+              setData(newData)
+          }).catch(err => {
+              console.log(err)
+          })
+  }
+
+  const makeComment = (text, postId) => {
+      fetch('/comment', {
+          method: "put",
+          headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + localStorage.getItem("jwt")
+          },
+          body: JSON.stringify({
+              postId,
+              text
+          })
+      }).then(res => res.json())
+          .then(result => {
+              console.log(result)
+              const newData = data.map(item => {
+                  if (item._id == result._id) {
+                      return result;
+                  }
+                  else {
+                      return item;
+                  }
+              })
+              setData(newData)
+              setcommentValue("")
+          }).catch(err => {
+              console.log(err)
+          })
+  }
+
+  const deletePost = (postid) => {
+      fetch(`/deletepost/${postid}`, {
+          method: "delete",
+          headers: {
+              Authorization: "Bearer " + localStorage.getItem("jwt")
+          }
+      }).then(res => res.json())
+          .then(result => {
+              console.log(result)
+              const newData = data.filter(item => {
+                  return item._id !== result._id
+              })
+              setData(newData)
+          })
+  }
   return (
     <div>
       <div className="add" style={{ backgroundColor: "#000000" }}>
@@ -28,6 +141,56 @@ const Lgbtq = () => {
           <br />
           <button className='btn'>Find a therapist</button></div>
       </div>
+      <hr/>
+            <h3 style={{ textAlign: "center" }}>SUCCESS STORIES THAT MAY INSPIRE YOU!!</h3>
+
+            <div className="home">
+                {
+                    data.map(item => {
+                        return (
+
+                            <div className="card home-card" key={item._id}>
+                                <h5><Link to={item.postedBy._id !== state._id ? "/profile/" + item.postedBy._id : "/profile"} >{item.postedBy.name}</Link> {item.postedBy._id == state._id
+                                    && <i className="material-icons" style={{
+                                        float: "right"
+                                    }}
+                                        onClick={() => deletePost(item._id)}
+                                    >delete</i>
+                                }</h5>
+                                <div className="card-image">
+                                        <img src={item.photo} alt={item.title}/>
+                                </div>
+                                <div className="card-content">
+                                    <i className="material-icons" style={{ color: "red" }}>favorite</i>
+                                    {item.likes.includes(state._id)
+                                        ?
+                                        <i className="material-icons" onClick={() => { unlikePost(item._id) }}>thumb_down</i>
+                                        :
+                                        <i className="material-icons" onClick={() => { likePost(item._id) }}>thumb_up</i>
+                                    }
+                                    <h6>{item.likes.length} likes</h6>
+                                    <h6>{item.title}</h6>
+                                    <p>{item.body}</p>
+                                    {
+                                        item.comments.map(record => {
+                                            return (
+                                                <h6 key={record._id}><span style={{ fontWeight: "500" }}>{record.postedBy.name} </span>{record.text}</h6>
+                                            )
+                                        })
+                                    }
+                                    <form onSubmit={(e) => {
+                                        e.preventDefault()
+                                        makeComment(e.target[0].value, item._id)
+                                    }}>
+                                        <input value={commentValue} onChange={(e) => setcommentValue(e.target.value)} type="text" placeholder="add a comment" />
+                                    </form>
+                                </div>
+                            </div>
+
+                        )
+                    })
+                }
+            </div>
     </div>
   )
 }
