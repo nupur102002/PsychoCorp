7d6613a252a77e8ff674ef2e5e06db46721de127
Nupur Goyal <nupur102002@gmail.com>
2023-03-25 21:32:08 +0530

    profile related routes fetch from backend

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -2,7 +2,63 @@ import React, { useEffect, useState, useContext } from "react";
 import { UserContext } from "../../App";
 
 const Profile = () => {
+    const [mypics, setPics] = useState([])
+    const { state, dispatch } = useContext(UserContext)
+    const [image, setImage] = useState("")
+    useEffect(() => {
+        fetch('/mystory', {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            }
+        }).then(res => res.json())
+            .then(result => {
+                console.log(result);
+                setPics(result.mystory)
+            })
+    }, [])
+     
 
+
+    useEffect(() => {
+        if (image) {
+            const data = new FormData()
+            data.append("file", image)
+            data.append("upload_preset", "zaayka")
+            data.append("cloud_name", "dkp8phxth")
+            fetch("https://api.cloudinary.com/v1_1/dkp8phxth/image/upload", {
+                method: "post",
+                body: data
+            })
+                .then(res => res.json())
+                .then(data => {
+
+
+                    fetch('/updatepic', {
+                        method: "put",
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": "Bearer " + localStorage.getItem("jwt")
+                        },
+                        body: JSON.stringify({
+                            pic: data.url
+                        })
+                    }).then(res => res.json())
+                        .then(result => {
+                            console.log(result)
+                            localStorage.setItem("user", JSON.stringify({ ...state, pic: result.pic }))
+                            dispatch({ type: "UPDATEPIC", payload: result.pic })
+                            window.location.reload()
+                        })
+
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    }, [image])
+    const updatePhoto = (file) => {
+        setImage(file)
+    }
     
     return (
         <div style={{ maxWidth: "550px", margin: "0px auto" }}>
@@ -23,9 +79,9 @@ const Profile = () => {
                     {/* name part */}
                 </div>
                 <div>
-                    {/* <h4>{state ? state.name : "loading"}</h4> */}
+                    <h4>{state ? state.name : "loading"}</h4>
                     <div>
-                        {/* <h6>Following {state ? state.following.length : "0"} Doctor</h6> */}
+                        <h6>Following {state ? state.following.length : "0"} Doctor</h6>
 
                     </div>
                 </div>
@@ -34,7 +90,7 @@ const Profile = () => {
             <div className="file-field input-field" style={{ margin: "10px" }}>
                 <div className="btn #64b5f6 blue darken-1">
                     <span>Update pic</span>
-                    {/* <input type="file" onChange={(e) => updatePhoto(e.target.files[0])} /> */}
+                    <input type="file" onChange={(e) => updatePhoto(e.target.files[0])} />
                 </div>
                 <div className="file-path-wrapper">
                     <input className="file-path validate" type="text" />
@@ -42,7 +98,7 @@ const Profile = () => {
             </div>
 
             {/* for pic uploaded part */}
-            {/* <div className="gallery">
+            <div className="gallery">
                 {
                     mypics.map(item => {
                         return (
@@ -50,7 +106,7 @@ const Profile = () => {
                         )
                     })
                 }
-            </div> */}
+            </div>
         </div>
     )
 }
