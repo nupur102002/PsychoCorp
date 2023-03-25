eebdcfe10587275a2507e5ead770ac7012c553c5
Nupur Goyal <nupur102002@gmail.com>
2023-03-25 18:32:46 +0530

    comment on story route

--- server/routes/story.js
+++ server/routes/story.js
@@ -90,4 +90,26 @@ router.put('/unlike',requireLogin,(req,res)=>{
       }
     })
 })
+
+//Comment on a story
+router.put("/comment",requireLogin,(req,res)=>{
+    const comment={
+     text:req.body.text,
+     postedBy:req.user._id
+    }
+    Story.findByIdAndUpdate(req.body.postId,{
+         $push:{comments:comment}
+     },{
+         new:true
+     })
+     .populate("comments.postedBy","_id name")
+     .populate("postedBy","_id name")
+     .exec((err,result)=>{
+         if(err){
+             return res.status(422).json({error:err})
+         }else{
+             res.json(result)
+         }
+     })
+  })
 module.exports=router;
