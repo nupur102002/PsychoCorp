70b4f730b173abb4da2553646e35a7f65f9d4df8
Nupur Goyal <nupur102002@gmail.com>
2023-03-26 02:43:35 +0530

    show depression category doctors

--- client/src/components/screens/Depression.js
+++ client/src/components/screens/Depression.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useContext } from "react";
 import { UserContext } from '../../App';
 import { Link } from "react-router-dom";
+import { Row, Col } from "antd"
 import M from "materialize-css"
 
-const Depression=()=> {
+const Depression = () => {
     const [data, setData] = useState([])
     const [commentValue, setcommentValue] = useState("")
     const { state, dispatch } = useContext(UserContext)
+    const [doc, setDoc] = useState([])
+
     useEffect(() => {
         fetch('/getdepression', {
             headers: {
@@ -16,6 +19,18 @@ const Depression=()=> {
             .then(result => {
                 // console.log(result)
                 setData(result.stories)
+            })
+    }, [])
+
+    useEffect(() => {
+        fetch('/getdepressiondoc', {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            }
+        }).then(res => res.json())
+            .then(result => {
+                console.log(result)
+                setDoc(result.doctors)
             })
     }, [])
 
@@ -117,28 +132,50 @@ const Depression=()=> {
                 setData(newData)
             })
     }
-  return (
-    <div>
-      <div className="add" style={{ backgroundColor: "#673366" }}>
-        <div><h2>Depression Community Members</h2>
-          <p>Meet your<br />new Community!!</p></div>
-        <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdCulqKw0otOlYoPd0W53vM_pwapWPRRNRk1yjfllzCA&usqp=CAU&ec=48665698" /></div>
-      </div>
-      <div className="add1">
-        <div style={{ width: "500px", paddingLeft: "20px" }}><h4>HELLO.<br />NICE TO<br />MEET YOU</h4></div>
-        <div><h5>Peer-to-peer support group for individuals living with the daily challenge of depression or bipolar disorder.</h5>
-          <p>Social support is critical to depression recovery. Depression and bipolar disorder, by their nature, are isolating illnesses. Connecting with individuals who understand firsthand can be an important step toward cultivating a new healthier life.</p></div>
-      </div>
-      <div className="add2">
-        <div style={{ paddingLeft: "20px" }}><h5>ABOUT DEPRESSION.</h5>
-          <p>On occasion everyone feels sad or blue, but these feelings are usually temporary –lasting only a few days. If feelings of depression last for an extended period of time, interfere with daily life and normal functioning you may be suffering from a depressive disorder. Depression is a common but serious illness, and most who experience it need some form of help or treatment to get better.
+    return (
+        <div>
+            <div className="add" style={{ backgroundColor: "#673366" }}>
+                <div><h2>Depression Community Members</h2>
+                    <p>Meet your<br />new Community!!</p></div>
+                <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdCulqKw0otOlYoPd0W53vM_pwapWPRRNRk1yjfllzCA&usqp=CAU&ec=48665698" /></div>
+            </div>
+            <div className="add1">
+                <div style={{ width: "500px", paddingLeft: "20px" }}><h4>HELLO.<br />NICE TO<br />MEET YOU</h4></div>
+                <div><h5>Peer-to-peer support group for individuals living with the daily challenge of depression or bipolar disorder.</h5>
+                    <p>Social support is critical to depression recovery. Depression and bipolar disorder, by their nature, are isolating illnesses. Connecting with individuals who understand firsthand can be an important step toward cultivating a new healthier life.</p></div>
+            </div>
+            <div className="add2">
+                <div style={{ paddingLeft: "20px" }}><h5>ABOUT DEPRESSION.</h5>
+                    <p>On occasion everyone feels sad or blue, but these feelings are usually temporary –lasting only a few days. If feelings of depression last for an extended period of time, interfere with daily life and normal functioning you may be suffering from a depressive disorder. Depression is a common but serious illness, and most who experience it need some form of help or treatment to get better.
 
-            Depression affects close to 10% of the population. Women experience depression at roughly twice the rate of men. However, intensive research into the illness has resulted in the development of medications, psychotherapies, as well as a variety of self-help techniques that have proven to be very effective in the treatment of depression.</p></div>
-        <div><img style={{ width: "300px", height: "200px", paddingRight: "20px" }} src="https://support.therapytribe.com/wp-content/uploads/2015/07/Depression-Full.jpg" />
-          <br />
-          <button className='btn'>Find a therapist</button></div>
-      </div>
-      <hr/>
+                        Depression affects close to 10% of the population. Women experience depression at roughly twice the rate of men. However, intensive research into the illness has resulted in the development of medications, psychotherapies, as well as a variety of self-help techniques that have proven to be very effective in the treatment of depression.</p></div>
+                <div><img style={{ width: "300px", height: "200px", paddingRight: "20px" }} src="https://support.therapytribe.com/wp-content/uploads/2015/07/Depression-Full.jpg" />
+                    <br />
+
+                </div>
+            </div>
+            <hr />
+            <div>
+                <Row justify="center mt-1" gutter={16}>
+                    {doc.map((doctor) => {
+                        return (
+                            <Col lg={5} sm={24} xs={24}>
+                                <Link to="/doctorprofile/{doctor.id}"><div className="car p-2 bs1 mt-3">
+                                    <div><img style={{ borderRadius: "50%", width: "200px", paddingLeft: "50px" }} src={doctor.pic} alt="a doctor" className="carimg" /></div>
+
+                                    <div className="car-content d-flex align-items-center justify-content-between">
+                                        <div>
+                                            <p className="pa" align="center">{doctor.name}</p>
+                                            <p className="pa" align="center">{doctor.body}</p>
+                                        </div>
+                                    </div>
+                                </div></Link>
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </div>
+            <hr />
             <h3 style={{ textAlign: "center" }}>SUCCESS STORIES THAT MAY INSPIRE YOU!!</h3>
 
             <div className="home">
@@ -155,7 +192,7 @@ const Depression=()=> {
                                     >delete</i>
                                 }</h5>
                                 <div className="card-image">
-                                        <img src={item.photo} alt={item.title}/>
+                                    <img src={item.photo} alt={item.title} />
                                 </div>
                                 <div className="card-content">
                                     <i className="material-icons" style={{ color: "red" }}>favorite</i>
@@ -188,8 +225,8 @@ const Depression=()=> {
                     })
                 }
             </div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Depression;
