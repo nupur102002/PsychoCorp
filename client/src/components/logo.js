af4f431085a1f5ea024625f8f09262fdd73aacad
Harsh311003 <harshsharma311003@gmail.com>
2023-03-25 15:31:05 +0530

    Adding signup page &connected to backend

---
+++ client/src/components/logo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+const Logo =()=>{
+    return (
+        <>
+            <div className='logo-holder'>
+                <div className='logo'>
+                <i class="ri-mental-health-fill"></i>
+                </div>
+            </div>
+           
+        </>
+    )
+}
+
+export default Logo;
