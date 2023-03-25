6246dcbc929031f310062f7e21143c7daf03ecd3
Nupur Goyal <nupur102002@gmail.com>
2023-03-25 17:38:43 +0530

    Daily doing activities

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -46,6 +46,59 @@ const Home = () => {
                 </div>
 
             </div>
+            <hr />
+            <hr />
+            <div className="container">
+                <div className="box1">
+                    <img src="https://d3t7uih9heml08.cloudfront.net/3562dbf3dce145ed6739c9672424b5e3.jpg" />
+                    <h6 align="center">Sweat</h6>
+                    <p align="center">Walk, run, cycle, or burpee your way to success.</p>
+                </div>
+                <div className="box1">
+                    <img src="https://d3t7uih9heml08.cloudfront.net/09410ded176a738263876a58e9242791.jpg" />
+                    <h6 align="center">Host</h6>
+                    <p align="center">Grab your friends and family and host an event</p>
+                </div>
+                <div className="box1">
+                    <img src="https://d3t7uih9heml08.cloudfront.net/3b842135cd52a8b80150bc2f02b2c858.jpg" />
+                    <h6 align="center">Remember</h6>
+                    <p align="center">Honour your loved ones</p>
+                </div>
+            </div>
+            <div className="container">
+                <div className="box1">
+                    <img src="https://d3t7uih9heml08.cloudfront.net/a415e34dca3f900bc5925603c09b3f31.jpg" />
+                    <h6 align="center">Change</h6>
+                    <p align="center">Cut your hair, shave your head and grow a mullet</p>
+                </div>
+                <div className="box1">
+                    <img src="https://d3t7uih9heml08.cloudfront.net/e531cfc4f550faed78e4280938cb18c2.jpg" />
+                    <h6 align="center">Stream</h6>
+                    <p align="center">Play your favorite game</p>
+                </div>
+                <div className="box1">
+                    <img src="https://d3t7uih9heml08.cloudfront.net/8fc56859606070fd9bb7868ea9b5d5cf.jpg" />
+                    <h6 align="center">Celebrate</h6>
+                    <p align="center">Celebrate every special day</p>
+                </div>
+            </div>
+            <div className="container">
+                <div className="box1">
+                    <img src="https://d3t7uih9heml08.cloudfront.net/6f134c484a03b05907b36ee3703c85d1.jpg" />
+                    <h6 align="center">Work</h6>
+                    <p align="center">Enjoy your work</p>
+                </div>
+                <div className="box1">
+                    <img src="https://d3t7uih9heml08.cloudfront.net/c1540948213bdaf98f33488376245027.jpg" />
+                    <h6 align="center">Resist</h6>
+                    <p align="center">Give up a bad habit for good cause</p>
+                </div>
+                <div className="box1">
+                    <img src="https://d3t7uih9heml08.cloudfront.net/92e6281e4fc9c7306a310cc5597cb16c.jpg" />
+                    <h6 align="center">Donate</h6>
+                    <p align="center">Donate a portion of your day to others</p>
+                </div>
+            </div>
         </div>
     )
 }
