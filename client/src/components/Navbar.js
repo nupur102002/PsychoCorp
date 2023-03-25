d2e2fec53b7a46d7c37383b63c35ce42bc34798f
Harsh311003 <harshsharma311003@gmail.com>
2023-03-25 18:03:52 +0530

    logout feature created

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -22,7 +22,12 @@ const NavBar = () => {
         <li key="3"><Link to="/createSuccess">Create Success Story</Link></li>,
           <li key="4">
             <button className="btn #c62828 red darken-3"
-             >
+              onClick={() => {
+                localStorage.clear()
+                dispatch({ type: "CLEAR" })
+                navigate('/health')
+                window.location.reload();
+              }}>
               Logout
             </button>
           </li>
@@ -37,7 +42,12 @@ const NavBar = () => {
           <li key="4"><Link to="/my ">Prev paitents</Link></li>,
           <li key="5">
             <button className="btn #c62828 red darken-3"
-              >
+              onClick={() => {
+                localStorage.clear()
+                dispatch({ type: "CLEAR" })
+                navigate('/health')
+                window.location.reload();
+              }}>
               Logout
             </button>
           </li>
