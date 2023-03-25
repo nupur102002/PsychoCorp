21d97c382be2ea331f78bd2365e6683b14291c65
Nupur Goyal <nupur102002@gmail.com>
2023-03-25 20:46:17 +0530

    show story on anxiety page

--- client/src/components/screens/Anxiety.js
+++ client/src/components/screens/Anxiety.js
@@ -141,7 +141,56 @@ const Anxiety=()=> {
           <br />
           <button className='btn'>Find a therapist</button></div>
       </div>
-            
+      <hr/>
+            <h3 style={{ textAlign: "center" }}>SUCCESS STORIES THAT MAY INSPIRE YOU!!</h3>
+
+            <div className="home">
+                {
+                    data.map(item => {
+                        return (
+
+                            <div className="card home-card" key={item._id}>
+                                <h5><Link to={item.postedBy._id !== state._id ? "/profile/" + item.postedBy._id : "/profile"} >{item.postedBy.name}</Link> {item.postedBy._id == state._id
+                                    && <i className="material-icons" style={{
+                                        float: "right"
+                                    }}
+                                        onClick={() => deletePost(item._id)}
+                                    >delete</i>
+                                }</h5>
+                                <div className="card-image">
+                                        <img src={item.photo} alt={item.title}/>
+                                </div>
+                                <div className="card-content">
+                                    <i className="material-icons" style={{ color: "red" }}>favorite</i>
+                                    {item.likes.includes(state._id)
+                                        ?
+                                        <i className="material-icons" onClick={() => { unlikePost(item._id) }}>thumb_down</i>
+                                        :
+                                        <i className="material-icons" onClick={() => { likePost(item._id) }}>thumb_up</i>
+                                    }
+                                    <h6>{item.likes.length} likes</h6>
+                                    <h6>{item.title}</h6>
+                                    <p>{item.body}</p>
+                                    {
+                                        item.comments.map(record => {
+                                            return (
+                                                <h6 key={record._id}><span style={{ fontWeight: "500" }}>{record.postedBy.name} </span>{record.text}</h6>
+                                            )
+                                        })
+                                    }
+                                    <form onSubmit={(e) => {
+                                        e.preventDefault()
+                                        makeComment(e.target[0].value, item._id)
+                                    }}>
+                                        <input value={commentValue} onChange={(e) => setcommentValue(e.target.value)} type="text" placeholder="add a comment" />
+                                    </form>
+                                </div>
+                            </div>
+
+                        )
+                    })
+                }
+            </div> 
     </div>
   )
 }
